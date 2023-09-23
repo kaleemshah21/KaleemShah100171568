11079161db0e3ba9f935a118d9aee5403de35fe1
shahk <shahk@DESKTOP-O7LU3RI.home>
2023-09-23 07:35:03 +0100

    tasks 1-9 for week 1 (not fully completed)

---
+++ JavaApplication1/src/javaapplication1/Guide_For_Using_Helper_Package.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+import helpers.*;
+import java.io.*;
+//NOT MANAGED TO GET WORKING.
+
+/**
+ *
+ * @author shahk
+ */
+public class Guide_For_Using_Helper_Package {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
