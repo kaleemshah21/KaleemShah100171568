307dc29dec066df7f2d7e46815a7f5de2b9816ea
shahk <shahk@DESKTOP-O7LU3RI.home>
2023-09-25 22:59:26 +0100

    tasks 1-9 for week 1 (mostly completed, a few errors)

--- JavaApplication1/src/javaapplication1/W1Ex9.java
+++ JavaApplication1/src/javaapplication1/W1Ex9.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication1;
-
+import java.util.Scanner;
 /**
  *
  * @author shahk
@@ -14,7 +14,16 @@ public class W1Ex9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("enter the money in savings account: ");
+        double amountStart = myObj.nextDouble();
+        System.out.println("enter the number of months");
+        int months = myObj.nextInt();
+        double amountEnd = amountStart * 0.01 * months;
+        System.out.print("the amount at the end of the time period: ");
+        System.out.print(amountEnd+amountStart);
+        
+        
     }
     
 }
