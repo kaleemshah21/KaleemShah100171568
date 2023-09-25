d921156187263caa142b22b457cb1d42a6bec480
shahk <shahk@DESKTOP-O7LU3RI.home>
2023-09-25 23:13:13 +0100

    tasks 1-9 for week 1 (mostly completed, a few errors)

---
+++ JavaApplication1/src/javaapplication1/Song.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author shahk
+ */
+public class Song {
+    private String title;
+    private String artist;
+    private int plays;
+    
+    public song(String Title,String Artist,Int Plays){
+        this.title=Title;
+        this.artist=Artist;
+        this.plays=Plays;
+        
+    }
+    
+}
