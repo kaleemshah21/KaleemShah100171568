83fc92501fd384b0aacb0170eb5f80dd4d852896
shahk <shahk@DESKTOP-O7LU3RI.home>
2023-10-10 15:22:14 +0100

    Incl. Song class and program almost fully done

--- JavaApplication1/src/javaapplication1/OOP.java
+++ JavaApplication1/src/javaapplication1/OOP.java
@@ -49,8 +49,6 @@ public class OOP {
                 songs.add(new Song(songName, songArtist, songPlays));
                 
                 
-                
-                
             }
             else if (choice == 2){
                 System.out.println("Enter the title of the song you want to remove: ");
@@ -102,35 +100,6 @@ public class OOP {
         }
         
         
-        
-        //Song myObj = new Song("I Wonder", "Kanye West", 491481155);
-        //System.out.println(myObj.getTitle() + ", " + myObj.getArtist() + ", " +  myObj.getPlays());
-        //myObj.setArtist("Kendrick Lamar"); //here i changed the artist to kendrick lamar and then printed the getArtist method which shows it has been changed
-        //System.out.println(myObj.getArtist());
-        
-        
-//        // Specify the title of the song you want to update
-//        String targetTitle = "I Wonder";
-//
-//        // Specify the new title you want to set
-//        String newTitle = "New Song Title";
-//
-//        // Search for the specific song by title and update its title
-//        for (Song song : songs) {
-//            if (song.getTitle().equals(targetTitle)) {
-//                // Found the song with the specified title
-//                song.setTitle(newTitle); // Update the title
-//                break; // Exit the loop since we found the song
-//            }
-//        }
-//
-//        // Print the updated list of songs to verify the title change
-//        for (Song song : songs) {
-//            System.out.println("Title: " + song.getTitle());
-//            System.out.println("Artist: " + song.getArtist());
-//            System.out.println("Plays: " + song.getPlays());
-//            System.out.println();
-//        }
     }
     
 }
