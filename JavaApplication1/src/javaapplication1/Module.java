c58d4c3742fbe9a6d1470d0bbf94bf3fe6e5494a
shahk <shahk@DESKTOP-O7LU3RI.home>
2023-10-12 20:56:49 +0100

    Incl. W4Ex1

---
+++ JavaApplication1/src/javaapplication1/Module.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author shahk
+ */
+public class Module {
+    private String moduleName;
+    private String moduleCode;
+    
+    
+    public Module(String moduleName, String moduleCode){
+        this.moduleName = moduleName;
+        this.moduleCode = moduleCode;
+    }
+}
